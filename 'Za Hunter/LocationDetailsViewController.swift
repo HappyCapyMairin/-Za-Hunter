29595fd6059b051b57124fa6827379cc84d40331
Mairin <mairincastellano@gmail.com>
2020-08-03 14:23:21 -0500

    Finished setting up Location Details View Controller

--- 'Za Hunter/LocationDetailsViewController.swift
+++ 'Za Hunter/LocationDetailsViewController.swift
@@ -12,8 +12,24 @@ class LocationDetailsViewController: UIViewController {
     
     var selectedMapItem = MKMapItem()
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+           nameLabel.text = selectedMapItem.placemark.name
+           var address = selectedMapItem.placemark.subThoroughfare! + " "
+           address += selectedMapItem.placemark.thoroughfare! + "\n"
+           address += selectedMapItem.placemark.locality! + ", "
+           address += selectedMapItem.placemark.administrativeArea! + " "
+           address += selectedMapItem.placemark.postalCode!
+           addressLabel.text = address
+           phoneLabel.text = selectedMapItem.phoneNumber
+       }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(selectedMapItem.)
+        print(selectedMapItem.name!)
     }
 }
+
